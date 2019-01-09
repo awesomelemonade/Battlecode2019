a8bd754b945a19957b57c1a0056a868c0cf8ae9b
awesomelemonade <awesomelemonade@gmail.com>
2019-01-08 23:02:41 -0500

    Added basic spawning of pilgrims & fixed encode/decode of positions in Util

--- bot/CastleBot.js
+++ bot/CastleBot.js
@@ -1,9 +1,9 @@
 import {SPECS} from 'battlecode'
 import * as Util from './Util';
+import {Vector} from './Library';
 
 var initialized = false;
 var isLeader = false;
-var step = -1;
 
 const LEADER_SIGNAL = 1;
 
@@ -26,19 +26,28 @@ function initialize(robot) {
     }
     
     // TODO: Figure out best castle to start with
+    
+    // BFS for some karbonite/fuel positions
+}
+var test = false;
+var action = undefined;
+
+function spawnPilgrim(robot) {
+    action = robot.buildUnit(SPECS.PILGRIM, 1, 1); // TODO: Face towards target
+    // Radio pilgrim's target position
+    robot.signal(Util.encodePosition(new Vector(12, 34)), 1); // TODO: Actually calculate a position
 }
 
 export function castleTurn(robot) {
+    action = undefined;
     if (!initialized) {
         initialize(robot);
         initialized = true;
     }
-    step++;
     robot.log("A Castle Turn");
-    if (step % 10 === 0) {
-        robot.log("Building a crusader at " + (robot.me.x+1) + ", " + (robot.me.y+1));
-        return robot.buildUnit(SPECS.CRUSADER, 1, 1);
-    } else {
-        return // this.log("Castle health: " + this.me.health);
+    if (isLeader && !test) {
+        spawnPilgrim(robot);
+        test = true;
     }
+    return action;
 }
--- bot/Util.js
+++ bot/Util.js
@@ -5,9 +5,9 @@ const BITMASK = 0b111111;
 
 export function encodePosition(position) {
     // position.x and position.y should have domain [0, 63]: 2^6-1
-    return ((position.x << X_SHIFT) & BITMASK) | (position.y & BITMASK);
+    return ((position.x & BITMASK) << X_SHIFT) | (position.y & BITMASK);
 }
 
 export function decodePosition(encodedPosition) {
-    return new Library.Vector(encodedPosition >>> X_SHIFT & BITMASK, encodedPosition & BITMASK);
+    return new Library.Vector((encodedPosition >>> X_SHIFT) & BITMASK, encodedPosition & BITMASK);
 }
