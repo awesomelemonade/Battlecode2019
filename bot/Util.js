b3ea679b0b25ab437e449ec1fcd19d5dc4c91307
awesomelemonade <awesomelemonade@gmail.com>
2019-01-10 13:15:10 -0500

    Added isHorizontallySymemetric() and isVerticallySymmetric() to Util

--- bot/Util.js
+++ bot/Util.js
@@ -11,3 +11,27 @@ export function encodePosition(position) {
 export function decodePosition(encodedPosition) {
 	return new Library.Vector((encodedPosition >>> X_SHIFT) & BITMASK, encodedPosition & BITMASK);
 }
+
+// Pass in boolean array
+export function isHorizontallySymmetric(array) {
+	for (var x = 0; x < array.length; x++) {
+		for (var y = 0; y < array[0].length / 2; y++) {
+			if (array[x][y] !== array[x][array[0].length - y - 1]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+// Pass in boolean array
+export function isVerticallySymmetric(array) {
+	for (var x = 0; x < array.length / 2; x++) {
+		for (var y = 0; y < array[0].length; y++) {
+			if (array[x][y] !== array[array.length - x - 1][y]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
