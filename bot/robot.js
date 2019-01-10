a308d5c8fe225525d8198b93b7e89470a51edad0
awesomelemonade <awesomelemonade@gmail.com>
2019-01-09 22:22:45 -0500

    Transposed all maps

--- bot/robot.js
+++ bot/robot.js
@@ -15,6 +15,11 @@ class MyRobot extends BCAbstractRobot {
 		this.bots[SPECS.CRUSADER] = crusaderTurn;
 	}
 	turn() {
+		// Transpose everything
+		this.map[0].map((col, i) => this.map.map(row => row[i]));
+		this.karbonite_map[0].map((col, i) => this.karbonite_map.map(row => row[i]));
+		this.fuel_map[0].map((col, i) => this.fuel_map.map(row => row[i]));
+		// Execute turn
 		for (var property in this.bots) {
 			if (this.bots.hasOwnProperty(property) && this.me.unit.toString() === property) {
 				return this.bots[property](this); // Call the turn
