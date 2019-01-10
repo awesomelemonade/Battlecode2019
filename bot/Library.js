b3f47e2ffba929fb23f4a889f4324d558daa6b8b
awesomelemonade <awesomelemonade@gmail.com>
2019-01-10 12:02:21 -0500

    Progress on moves & moveCosts in Library

--- bot/Library.js
+++ bot/Library.js
@@ -10,3 +10,40 @@ export class Vector {
 		return this.x * 9999 + this.y;
 	}
 }
+
+export var totalMoves = [];
+export var totalMoveCosts = [];
+
+function addMove(x, y, cost) {
+	totalMoves.push(new Vector(x, y));
+	totalMoveCosts.push(cost);
+}
+function addMoves(radius) {
+	for (var x = 1; x <= radius; x++) {
+		// Cardinal Directions = x * x
+		var c = x * x;
+		addMove(x, 0, c);
+		addMove(0, x, c);
+		addMove(-x, 0, c);
+		addMove(0, -x, c);
+		// Non cardinal directions or diagonals
+		for (var y = 1; y < x; y++) {
+			var cost = c + y * y;
+			addMove(x, y, cost);
+			addMove(y, x, cost);
+			addMove(-x, y, cost);
+			addMove(-y, x, cost);
+			addMove(x, -y, cost);
+			addMove(y, -x, cost);
+			addMove(-x, -y, cost);
+			addMove(-y, -x, cost);
+		}
+		// Diagonals = 2 * x * x
+		c = 2 * c;
+		addMove(x, x, c);
+		addMove(-x, x, c);
+		addMove(x, -x, c);
+		addMove(-x, -x, c);
+	}
+}
+addMoves(10);
