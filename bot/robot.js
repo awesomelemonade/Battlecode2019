301daf16c452dcce13fa4dbde67e1de63de444c3
awesomelemonade <awesomelemonade@gmail.com>
2019-01-10 22:24:43 -0500

    Added WrappedController to fix transposing

--- bot/robot.js
+++ bot/robot.js
@@ -5,6 +5,25 @@ import {pilgrimTurn} from './bot/PilgrimBot';
 import {crusaderTurn} from './bot/CrusaderBot';
 import {churchTurn} from './bot/ChurchBot';
 
+
+class WrappedController {
+	constructor(robot) {
+		var properties = ["move", "mine", "give", "attack", "buildUnit", "proposeTrade", "signal", "castleTalk", "log", "getVisibleRobots", "getVisibleRobotMap", "getRobot", "isVisible", "isRadioing"];
+		for (var i = 0; i < properties.length; i++) {
+			this[properties[i]] = robot[properties[i]].bind(robot);
+		}
+		this.turnProperties = ["me", "karbonite", "fuel", "last_offer"];
+		this.map = robot.map[0].map((col, i) => robot.map.map(row => row[i]));
+		this.karbonite_map = robot.karbonite_map[0].map((col, i) => robot.karbonite_map.map(row => row[i]));
+		this.fuel_map = robot.fuel_map[0].map((col, i) => robot.fuel_map.map(row => row[i]));
+	}
+	turn() {
+		for (var i = 0; i < this.turnProperties.length; i++) {
+			this[this.turnProperties[i]] = robot[this.turnProperties[i]];
+		}
+	}
+}
+
 class MyRobot extends BCAbstractRobot {
 	constructor() {
 		super();
@@ -17,16 +36,14 @@ class MyRobot extends BCAbstractRobot {
 	}
 	turn() {
 		if (!this.initialized) {
-			// Transpose everything
-			this.map = this.map[0].map((col, i) => this.map.map(row => row[i]));
-			this.karbonite_map = this.karbonite_map[0].map((col, i) => this.karbonite_map.map(row => row[i]));
-			this.fuel_map = this.fuel_map[0].map((col, i) => this.fuel_map.map(row => row[i]));
+			this.controller = new WrappedController(this);
 			this.initialized = true;
 		}
+		this.controller.turn();
 		// Execute turn
 		for (var property in this.bots) {
 			if (this.bots.hasOwnProperty(property) && this.me.unit.toString() === property) {
-				return this.bots[property](this); // Call the turn
+				return this.bots[property](this.controller); // Call the turn
 			}
 		}
 	}
