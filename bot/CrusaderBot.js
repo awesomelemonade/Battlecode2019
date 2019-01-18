52d15ae0e88fc8e97d8353f1f2793a0ef10de962
awesomelemonade <awesomelemonade@gmail.com>
2019-01-17 21:11:42 -0500

    Converted CrusaderBot and PreacherBot to new bot format

--- bot/CrusaderBot.js
+++ bot/CrusaderBot.js
@@ -1,8 +1,18 @@
 
 
-export function crusaderTurn(robot) {
-	robot.log("A Crusader Turn");
-	const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
-	const choice = choices[Math.floor(Math.random()*choices.length)]
-	return robot.move(...choice);
+export class CrusaderBot {
+	constructor(controller) {
+		this.controller = controller;
+		init();
+	}
+	function init() {
+		
+	}
+	function turn() {
+		this.controller.log("A Crusader Turn");
+		const choices = [[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+		const choice = choices[Math.floor(Math.random() * choices.length)]
+		return this.controller.move(...choice);
+	}
 }
+
--- bot/PreacherBot.js
+++ bot/PreacherBot.js
@@ -1,5 +1,18 @@
 
 
-export function preacherTurn(robot) {
-	robot.log("A Preacher Turn");
+export class PreacherBot {
+	constructor(controller) {
+		this.controller = controller;
+		init();
+	}
+	function init() {
+		
+	}
+	function turn() {
+		this.controller.log("A Preacher Turn");
+		const choices = [[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+		const choice = choices[Math.floor(Math.random() * choices.length)]
+		return this.controller.move(...choice);
+	}
 }
+
