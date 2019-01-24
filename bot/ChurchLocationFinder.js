6294a34a0c7126043b3bd9626fa213515560194c
awesomelemonade <awesomelemonade@gmail.com>
2019-01-24 09:40:08 -0500

    Progress on ChurchLocationFinder optimization

--- bot/ChurchLocationFinder.js
+++ bot/ChurchLocationFinder.js
@@ -3,98 +3,110 @@ import {Vector, totalMoves, totalMoveCosts} from './Library'
 
 var UNEXPLORED = -1;
 
-export function resolve(controller, castlePositions) {
-	var map = controller.true_map;
-	var dist = new Array(map.length).fill().map(() => Array(map[0].length).fill(UNEXPLORED));
-	
+var controller;
+var castlePositions;
+var enemyCastlePredictions;
+var map;
+var validChurchLocation;
+var numResources;
+var resourceDistance;
+
+// TODO: handle updating structurePositions
+
+export function resolve(c, localCastlePositions, localEnemyCastlePredictions) {
+	controller = c;
+	castlePositions = localCastlePositions;
+	enemyCastlePredictions = localEnemyCastlePredictions;
+	map = controller.true_map;
+	validChurchLocation = new Array(map.length).fill().map(() => Array(map[0].length).fill(false));
+	numResources = new Array(map.length).fill().map(() => Array(map[0].length).fill(UNEXPLORED));
+	resourceDistance = new Array(map.length).fill().map(() => Array(map[0].length).fill(UNEXPLORED));
+	for (var i = 0; i < map.length; i++) {
+		for (var j = 0; j < map[0].length; j++) {
+			var location = new Vector(i, j);
+			var info = getChurchLocationInfo(location);
+			validChurchLocation[i][j] = info.valid;
+			if (validChurchLocation[i][j]) {
+				numResources[i][j] = info.numResources;
+				resourceDistance[i][j] = info.resourceDistance;
+			}
+		}
+	}
 }
 
 function findChurchLocation() {
 	// Use true_map for resource finding
-	var self = this;
 	// Do big bfs
-	var bigBfs = new Bfs(this.controller.true_map, this.castlePositions, totalMoves);
+	var bigBfs = new Bfs(controller.true_map, castlePositions, totalMoves);
 	var bigStop = bigBfs.resolve(function(location) {
-		return self.isValidChurchLocation(location);
+		return validChurchLocation[location.x][location.y];
 	});
 	if (bigStop === undefined) {
 		return undefined;
 	}
 	var traced = Util.trace(bigBfs, bigStop);
-	if (!traced.equals(Vector.ofRobotPosition(this.controller.me))) {
-		this.controller.log("Deferring creation of church pilgrim to castle at " + traced);
+	if (!traced.equals(Vector.ofRobotPosition(controller.me))) {
+		controller.log("Deferring creation of church pilgrim to castle at " + traced);
 		return null; // lol there has to be a better way
 	}
 	var bestChurchLocation = undefined;
 	var bestNumResources = 0;
 	var bestResourceDistance = 0;
-	var smallBfs = new Bfs(this.controller.true_map, bigStop, totalMoves);
+	var smallBfs = new Bfs(controller.true_map, bigStop, totalMoves);
 	smallBfs.resolve(function(location) { // Stop Condition
 		// Count the number of resources within responsible distance
-		var numResources = 0;
-		var resourceDistance = 0;
-		for (var i = -responsibleDistanceRadius; i <= responsibleDistanceRadius; i++) {
-			for (var j = -responsibleDistanceRadius; j <= responsibleDistanceRadius; j++) {
-				if (i * i + j * j > responsibleDistance) {
-					continue;
-				}
-				var v = new Vector(location.x + i, location.y + j);
-				if (!Util.outOfBounds(v)) {
-					if (Util.hasResource(v)) {
-						numResources++;
-						resourceDistance += i * i + j * j;
-					}
-				}
-			}
-		}
+		var currentNumResources = numResources[location.x][location.y];
+		var currentResourceDistance = resourceDistance[location.x][location.y];
 		// Compare with bestChurchLocation
-		if (numResources > bestNumResources) {
+		if (currentNumResources > bestNumResources) {
 			bestChurchLocation = location;
-			bestNumResources = numResources;
-			bestResourceDistance = resourceDistance;
-		} else if (numResources === bestNumResources && resourceDistance < bestResourceDistance) {
+			bestNumResources = currentNumResources;
+			bestResourceDistance = currentResourceDistance;
+		} else if (currentNumResources === bestNumResources && currentResourceDistance < bestResourceDistance) {
 			bestChurchLocation = location;
-			bestNumResources = numResources;
-			bestResourceDistance = resourceDistance;
+			bestNumResources = currentNumResources;
+			bestResourceDistance = currentResourceDistance;
 		}
 		return false; // Never stop until exhausted all unignored tiles
 	}, function(location) { // Ignore Condition
-		return !self.isValidChurchLocation(location);
+		return !validChurchLocation[location.x][location.y];
 	});
 	return bestChurchLocation;
 }
 
-function isValidChurchLocation(location) {
+function getChurchLocationInfo(location) {
 	// Ensure not on top of resource
 	if (Util.hasResource(location)) {
-		return false;
+		return {valid: false};
 	}
 	// Ensure not too close to other structures
-	for (var i = 0; i < this.structurePositions.length; i++) {
-		var position = this.structurePositions[i];
+	for (var i = 0; i < structurePositions.length; i++) {
+		var position = structurePositions[i];
 		if (location.getDistanceSquared(position) <= responsibleDistanceDoubled) {
-			return false;
+			return {valid: false};
 		}
 	}
 	// Ensure not near enemy castle predictions
-	for (var i = 0; i < this.enemyCastlePredictions.length; i++) {
-		var position = this.enemyCastlePredictions[i];
+	for (var i = 0; i < enemyCastlePredictions.length; i++) {
+		var position = enemyCastlePredictions[i];
 		if (location.getDistanceSquared(position) <= responsibleDistanceDoubled) {
-			return false;
+			return {valid: false};
 		}
 	}
 	// Check if there resources around the responsibleDistance
-	return hasResourceOrder(location);
-}
-
-
-function hasResourceOrder(position) {
 	var start = Util.getAdjacentPassable(position);
-	var bfs = new Bfs(this.controller.true_map, start, totalMoves);
-	return bfs.resolve(function(location) { // Stop condition
+	var bfs = new Bfs(controller.true_map, start, totalMoves, totalMoveCosts);
+	var numResources = 0;
+	var resourceDistance = 0;
+	bfs.resolve(function(location) { // Stop condition
 		// Stop condition is guaranteed to be evaluated only once per square
-		return Util.hasResource(location);
+		if (Util.hasResource(location)) {
+			numResources++;
+			resourceDistance += position.getDistanceSquared(location);
+		}
+		return false; // Never trigger the stop condition
 	}, function(location) { // Ignore condition
 		return position.getDistanceSquared(location) > responsibleDistance;
-	}) !== undefined;
+	});
+	return {valid: true, numResources: numResources, resourceDistance: resourceDistance};
 }
